94f42c29e72e1cfcc1de8e1191d6cd3da282d48b
wbiokr <Wbiokr@hotmail.com>
2017-10-19 16:15:41 +0800

    node服务器测试本地打包代码

--- http.js
+++ http.js
@@ -3,19 +3,24 @@ const url=require('url');
 const path=require('path');
 const fs=require('fs');
 var pn='';
+const hostLocal='localhost';
+const hostIpv4=require('ipv4');
+const port=8876;
+
 http.createServer((req,res)=>{
     let pathname=url.parse(req.url).pathname;
-	console.log(pathname)
-	pn=pathname
     if(pathname.indexOf('.')==-1){
         pathname+='/index.html';
     }
     let fileURL='./'+path.normalize('./build/'+pathname);
+    // let fileURL='./index.html';
+    console.log('地址为:'+fileURL)
+    // if(fileURL)
     let extname=path.extname(pathname);
     fs.readFile(fileURL,(Error,data)=>{
         if(Error){
             res.writeHead(404,{'Content-type':'text/html;charset=UTF8'});
-            res.end('404,页面没有找到！')
+            res.end('404,资源没有找到！')
         }
         getMime(extname,(mime)=>{
             res.writeHead(200,{'Content-type':mime});
@@ -23,11 +28,36 @@ http.createServer((req,res)=>{
         })
     })
     
-}).listen(8876,'localhost');
+}).listen(port,hostLocal);
+
+console.log(`we are listening:${hostLocal}:${port}`)
+
+http.createServer((req,res)=>{
+  let pathname=url.parse(req.url).pathname;
+  if(pathname.indexOf('.')==-1){
+      pathname+='/index.html';
+  }
+  let fileURL='./'+path.normalize('./build/'+pathname);
+  // let fileURL='./index.html';
+  console.log('地址为:'+fileURL)
+  // if(fileURL)
+  let extname=path.extname(pathname);
+  fs.readFile(fileURL,(Error,data)=>{
+      if(Error){
+          res.writeHead(404,{'Content-type':'text/html;charset=UTF8'});
+          res.end('404,资源没有找到！')
+      }
+      getMime(extname,(mime)=>{
+          res.writeHead(200,{'Content-type':mime});
+          res.end(data);
+      })
+  })
+}).listen(port,hostIpv4);
+console.log(`You can also access us by IP:${hostIpv4}:${port}`)
 
 
 function getMime(extname,callback){
-    fs.readFile('./mime.json',(err,data)=>{
+    fs.readFile('./config/mime.json',(err,data)=>{
         if(err){
             throw Error('sorry')
         };
