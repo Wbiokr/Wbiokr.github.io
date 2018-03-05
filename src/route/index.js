0db5b58d9b0a49dc266c7c0496edb77e930b0806
wbiokr <Wbiokr@hotmail.com>
2018-03-05 20:11:30 +0800

    style

--- src/route/index.js
+++ src/route/index.js
@@ -26,20 +26,20 @@ export default class Routes extends React.Component {
               className='navigator'
               path='/:any'
               component={NavBar}
-              atEnter={{offset:-100}}
-              atLeave={{offset:-150}}
-              atActive={{offset:0}}
-              mapStyle={(styles)=>{
-                transform:`translateY(${styles.offset})`
+              atEnter={{ offset: -100 }}
+              atLeave={{ offset: slide(-150) }}
+              atActive={{ offset: slide(0) }}
+              mapStyle={(styles) => {
+                transform: `translateY(${styles.offset})`
               }}
             />
-            <AnimatedSwitch 
+            <AnimatedSwitch
               className={`content ${content}`}
-              atEnter={{offset:50}}
-              atLeave={{offset:-100}}
-              atActive={{offset:0}}
-              mapStyle={(styles)=>{
-                transform:`translateX(${styles.offset})`
+              atEnter={{ offset: 50 }}
+              atLeave={{ offset: glide(-100) }}
+              atActive={{ offset: glide(0) }}
+              mapStyle={(styles) => {
+                transform: `translateX(${styles.offset})`
               }}
             >
               {
@@ -55,18 +55,39 @@ export default class Routes extends React.Component {
   }
 }
 
-const rule=cxs({
-  height:'100%',
-  width:'100%',
+const slide = (val) => {
+  return spring(
+    val,
+    {
+      stiffness: 125,
+      damping: 16
+    }
+  )
+}
+
+const glide=(val)=>{
+  return spring(
+    val,
+    {
+      stiffness:174,
+      damping:24
+    }
+  )
+}
+
+const rule = cxs({
+  height: '100%',
+  width: '100%',
 })
 
-const content=cxs({
-  position:'absolute',
-  height:'100%',
-  width:'100%',
-  zIndex:1,
-  ' >div':{
-    height:'100%',
-    width:'100%',
+const content = cxs({
+  position: 'absolute',
+  height: '100%',
+  width: '100%',
+  zIndex: 1,
+  ' >div': {
+    height: '100%',
+    width: '100%',
   }
 })
+
