beb8fba3a4dace5adf26fcee192b2c6c9a30ba95
wbiokr <Wbiokr@hotmail.com>
2018-03-06 16:07:12 +0800

    redux

--- src/redux/reducers/music.js
+++ src/redux/reducers/music.js
@@ -1,7 +1,20 @@
 const initState={
   total:200,
   label:['李志','天空之城','许巍','蓝莲花','南方姑娘','致英格兰友人demo'],
-  
+  apis:[
+    {
+      name:'网易',
+      id:'wy',
+    },
+    {
+      name:'酷狗',
+      id:'kg',
+    },
+    {
+      name:'酷我',
+      id:'kw'
+    }
+  ],
   songs:[
     {
       name:'天空之城',
